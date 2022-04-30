22684875036c6783302201e9366141cd4bb91a37
Ilya Bondarev <ilushkabond@gmail.com>
2022-04-30 16:05:22 +0200

    feat: eslint/airbnb

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,5 +1,62 @@
-import React, { useEffect, useState } from "react";
-import styles from './style.css'
+import React, { useEffect, useState } from 'react';
+import styles from './style.css';
+
+function FileViewer() {
+  //   const [dir, setDir] = useState({})
+  const [files, setFiles] = useState<Array<FileInfo>>([]);
+  const [path, setPath] = useState('/');
+  useEffect(() => {
+    fetch(`http://localhost:3000/dir?path=${path}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setFiles(res.files);
+      });
+  }, [path]);
+  return (
+    <div>
+      {
+        path !== '/' && (
+          <button
+            type="button"
+            className={styles.text}
+            onClick={() => {
+              setPath((lastPath) => {
+                const r = lastPath.split('/');
+                r.pop();
+                return r.join('/');
+              });
+            }}
+          >
+            <span>..</span>
+          </button>
+        )
+      }
+      {files.map((file) => (
+        <button
+          type="button"
+          className={styles.text}
+          onClick={() => {
+            if (file.isDirectory) {
+              setPath((lastPath) => `${lastPath}/${file.name}`);
+            } else {
+              fetch(`http://localhost:3000/download?path=${`${path}/${file.name}`}`)
+                .then((res) => res.blob())
+                .then((res) => {
+                  const a = document.createElement('a');
+                  a.download = file.name;
+                  a.href = URL.createObjectURL(res);
+                  a.click();
+                });
+            }
+          }}
+        >
+          <span>{file.name}</span>
+          <span>{file.isDirectory ? '...' : ''}</span>
+        </button>
+      ))}
+    </div>
+  );
+}
 
 export function App() {
   return <FileViewer />;
@@ -8,61 +65,4 @@ export function App() {
 interface FileInfo {
   isDirectory: boolean,
   name: string
-}
-
-function FileViewer() {
-  //   const [dir, setDir] = useState({})
-  const [files, setFiles] = useState<Array<FileInfo>>([]);
-  const [path, setPath] = useState("/");
-  useEffect(() => {
-    fetch(`http://localhost:3000/dir?path=${path}`)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        setFiles(res.files);
-      });
-  }, [path]);
-  return (
-    <div>
-      {
-        path != '/' && (
-          <div className={styles.text} onClick={() => {
-            setPath((path) => {
-              const r = path.split("/");
-              r.pop();
-              return r.join("/");
-            });
-          }}
-          >
-            <span>..</span>
-          </div>
-        )
-      }
-      {files.map((file) => {
-        return (
-          <div
-            className={styles.text}
-            onClick={() => {
-              if (file.isDirectory) {
-                setPath((path) => path + "/" + file.name);
-              } else {
-                fetch(`http://localhost:3000/download?path=${path + "/" + file.name}`)
-                .then((res) => res.blob())
-                .then((res) => {
-                  console.log(res);
-                  const a = document.createElement('a')
-                  a.download = file.name;
-                  a.href = URL.createObjectURL(res);
-                  a.click();
-                });
-              }
-            }}
-          >
-            <span>{file.name}</span>
-            <span>{file.isDirectory ? "..." : ""}</span>
-          </div>
-        );
-      })}
-    </div>
-  );
 }
